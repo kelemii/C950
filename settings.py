69a5830617e7c098bc1b0241ae5fe6f4990aae31
kelemii <98933496+kelemii@users.noreply.github.com>
2024-11-03 10:36:20 -0700

    moved some files around, added supporting documents provided

--- settings.py
+++ settings.py
@@ -8,9 +8,9 @@ clock = Clock()
 hub = "4001 South 700 East"
 pause_event = threading.Event()
 hash_table = PackageHashTable(40)
-csv_file_path = 'WGUPS Package File.csv'
-addresses_csv = 'WGUPS Address File.csv'
-distances_csv = 'WGUPS Distance Table.csv'
+csv_file_path = 'Files/WGUPS Package File.csv'
+addresses_csv = 'Files/WGUPS Address File.csv'
+distances_csv = 'Files/WGUPS Distance Table.csv'
 scheduled_time = clock.get_current_time().replace(hour=10, minute=20)
 new_address = "410 S. State St., Salt Lake City, UT 84111"
 updated_package_info = hash_table.lookup(9)
